e4659dadf022f95addffde4d48e49046e4caa698
lalawue <suchaaa@gmail.com>
2014-01-01 02:33:05 +0800

    expect script for halt server

---
+++ halt_server.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/expect
+#
+# create in 2013/12/31, by sucha in http://suchang.net
+#
+# Usage: halt_server host_ip
+
+# prompt for root@host_ip
+set serv_root_prompt "password"
+set serv_halt_cmd "shutdown -h now"
+set serv_halt_expect "halt"
+
+set timeout 10
+if { $argc != 1 } {
+	send_user "$argv0 \$HOST_IP\n"
+	exit
+}
+
+# get server ip
+set host [lrange $argv 0 0]
+
+# get root's passwd
+stty -echo
+send_user -- "Password for root@$host: "
+expect_user -re "(.*)\n"
+send_user "\n"
+set pass $expect_out(1,string)
+
+# login server and run halt
+spawn ssh -2 root@$host
+expect $serv_root_prompt
+send $pass\r
+send $serv_halt_cmd\r
+expect $serv_halt_expect
+exit
